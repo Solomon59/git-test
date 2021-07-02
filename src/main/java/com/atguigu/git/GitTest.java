cfb7a3af7c12100b3b7e37f3389e7c79100246e7
Solomon59 <86815490+Solomon59@users.noreply.github.com>
2021-07-02 17:10:29 +0800

    pull-commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
         System.out.println("master");
 
         System.out.println("hot-fix");
-        System.out.println("pull-fix");
+        System.out.println("push");
+        System.out.println("pull");
     }
 }
